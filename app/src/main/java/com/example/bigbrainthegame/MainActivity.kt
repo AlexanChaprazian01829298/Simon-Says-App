81f69d06f295f1cb2ac6a5b9daa88902a45135b0
Alexan <alexan_chaprazian@student.uml.edu>
2022-12-12 18:19:51 -0500

    added honor statement for final submission

--- app/src/main/java/com/example/bigbrainthegame/MainActivity.kt
+++ app/src/main/java/com/example/bigbrainthegame/MainActivity.kt
@@ -17,6 +17,42 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.bigbrainthegame.databinding.ActivityMainBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
+
+/***********************************************
+Authors: Alexan Chaprazian, Krestina Beshara
+Date: 12/12/22
+Purpose: Created a childrens game to boost the ability to memorize by implementing a
+version of simon says
+What Learned: learned how to use shared preferenfeces to display the high score even after the
+app is done running. Also learned how to add music to the app while the game is being played.
+Sources of Help: https://www.youtube.com/watch?v=NPYaUnGX6kw&ab_channel=MunirHoque
+https://developer.android.com/training/data-storage/shared-preferences
+https://www.youtube.com/watch?v=I-T_A9tnhgQ&ab_channel=Mr.Kaiser
+Time Spent (Hours): 74 hours
+ ***********************************************/
+/*
+Mobile App Development I -- COMP.4630 Honor Statement
+COMP.4630 Project F22 - Dr. Lin
+The practice of good ethical behavior is essential for
+maintaining good order in the classroom, providing an
+enriching learning experience for students, and training as
+a practicing computing professional upon graduation. This
+practice is manifested in the University's Academic
+Integrity policy. Students are expected to strictly avoid
+academic dishonesty and adhere to the Academic Integrity
+policy as outlined in the course catalog. Violations will
+be dealt with as outlined therein. All programming
+assignments in this class are to be done by the student
+alone unless otherwise specified. No outside help is
+permitted except the instructor and approved tutors.
+I certify that the work submitted with this assignment is
+mine and was generated in a manner consistent with this
+document, the course academic policy on the course website
+on Blackboard, and the UMass Lowell academic code.
+Date:12/12/22
+Names:Alexan Chaprazian & Krestina Beshara
+*/
+
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
